6b1b47bd84f03c455d5a4ebecf95dac48d0d3c3c
James <james@jeptechnology.com>
2021-10-15 13:32:17 +0100

    Fix

--- Examples/ColourCycle/main.cpp
+++ Examples/ColourCycle/main.cpp
@@ -2,28 +2,27 @@
 #include <unistd.h>
 
 #include "Button.h"
+#include "Led.h"
 
 using namespace std;
-
-void ButtonCallback(bool isPressed)
-{
-   cout << "Setup button is now " << (isPressed ? "pressed" : "released") << endl;
-}
 
 int main(int argc, char *argv[])
 {
    Button setup("setup", "/hardware/setupbutton/event1");
+   
    Led    red  ("red",   "/hardware/leds/status_red/pattern");
    Led    green("green", "/hardware/leds/status_green/pattern");
    Led    blue ("blue",  "/hardware/leds/status_blue/pattern");
 
 	cout << "Press setup button to switch led colour." << endl;
 
-   LED* currentLed = &red;
+   Led* currentLed = &red;
 
    // register a callback for when the button changes state
    setup.RegisterHandler([&] (bool isPressed) {
 
+      if (isPressed) return;
+      
       currentLed->TurnLedOff();
 
       if (currentLed == &red)        { currentLed = &blue;  }
